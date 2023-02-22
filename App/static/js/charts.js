61b8df7e591a3fb9dd4db4d827bd6514507c1212
Szymon Rogowski <szymon0r0official@gmail.com>
2023-02-22 19:05:21 +0100

    Async, await stuff for charts

--- App/static/js/charts.js
+++ App/static/js/charts.js
@@ -19,79 +19,117 @@ let labelsForBarChart = [];
 
 console.log("Before getData")
 let currValDict = [];
-function getRates(currency_name) {
-    if(currency_name === "pln"){
+
+async function getRates(currencyNames) {
+        if (currencyNames === "pln") {
         console.log("Curr = pln and Val = 1");
         return 1;
     }
-    return fetch(`http://api.nbp.pl/api/exchangerates/rates/a/${currency_name}`)
-        .then((response) => response.json())
-        .then(json_data => {
-            // console.log(json_data);
-            const value_in_pln = json_data.rates[0].mid;
-            console.log(`Curr = ${currency_name} and Val = ${ value_in_pln}`);
-            labelsForBarChart.push(currency_name);
-            valuesForBarChart.push(value_in_pln);
-            console.log(labelsForBarChart)
-            return {currency_name: value_in_pln};
-        })
-        .catch(error => {
-            console.error(error);
-        });
+      const result = {};
+    for (const currencyName of currencyNames) {
+        const response = await fetch(`http://api.nbp.pl/api/exchangerates/rates/a/${currencyName}`);
+        const data = await response.json();
+        const valueInPLN = data.rates[0].mid;
+        result[currencyName] = valueInPLN;
+  }
+    console.log("RESULT = " + result);
+    result["pln"] = 1;
+    return result;
+//     let currency_name = ""
+//     for (const key of Object.keys(dict)) {
+//         currency_name = dict[key];
+//
+//     if (currency_name === "pln") {
+//         console.log("Curr = pln and Val = 1");
+//         return 1;
+//     }
+//     try {
+//         const response = await fetch(`http://api.nbp.pl/api/exchangerates/rates/a/${currency_name}`);
+//         const json_data = await response.json();
+//         const value_in_pln = json_data.rates[0].mid;
+//         console.log(`Curr = ${currency_name} and Val = ${value_in_pln}`);
+//         labelsForBarChart.push(currency_name);
+//         valuesForBarChart.push(value_in_pln);
+//         console.log(labelsForBarChart)
+//         return {currency_name: value_in_pln};
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
 }
 
-
-for (const key of Object.keys(dict)) {
-    getRates(dict[key]);
+async function setter(){
+    const currencyNames = ["chf", "eur", "gbp", "usd"];
+    // const lolo = getRates(currencyNames)
+    // .then(result => {
+    //     console.log("lolo keys = " + Object.keys(result));
+    //     console.log("lolo values = " + Object.values(result));
+    // })
+    // .catch(error => {
+    //     console.error(error);
+    // });
+    const result = await getRates(currencyNames);
+    console.log(result);
+    const values = Object.values(result);
+    console.log("lolo values = " + values);
+    const keys = Object.keys(result);
+    console.log("lolo keys = " + keys);
+    // await getterValues(values);
+    // await getterKeys(keys);
+    return result;
 }
+setter()
 
-// labelsForBarChart.push("pln");
-// valuesForBarChart.push(1);
-console.log(labelsForBarChart);
-console.log("labelsForBarChart length = " + labelsForBarChart.length)
-console.log(valuesForBarChart);
-console.log("valuesForBarChart length = " + valuesForBarChart.length)
+// async function getterValues(argVal){
+//     console.log("getterValues = " + argVal);
+//     return argVal;
+// }
+//
+// async function getterKeys(argName){
+//     console.log("getterKeys = " + argName);
+//     return argName;
+// }
 
-console.log("After getData");
-
-
-
-
-
-
+//======================================================================================================================
 const ctxBarChart = document.getElementById('barChart');
 const ctxLineChart = document.getElementById('lineChart');
-
-
 
 const stars = [135850, 52122, 148825, 16939, 9763];
 const frameworks = ['React', 'Angular', 'Vue', 'Hyperapp', 'Omi'];
 
+async function charts() {
+    const result = await setter();
 
-//======================================================================================================================
-const barChart = new Chart(ctxBarChart, {
- type: 'bar',
- data: {
-    labels: labelsForBarChart,
-    datasets: [{
-        label: 'Github Stars',
-        data: valuesForBarChart,
-         backgroundColor: [
-             "rgba(255, 99, 132, 0.2)",
-             "rgba(54, 162, 235, 0.2)",
-             "rgba(255, 206, 86, 0.2)",
-             "rgba(75, 192, 192, 0.2)",
-             "rgba(153, 102, 255, 0.2)"],
-         borderColor: [
-             "rgba(255, 99, 132, 1)",
-             "rgba(54, 162, 235, 1)",
-             "rgba(255, 206, 86, 1)",
-             "rgba(75, 192, 192, 1)",
-             "rgba(153, 102, 255, 1)",],
-        borderWidth: 1
-    }]
- },
-})
+    console.log("CHARTS result = " + result);
+
+    const values = Object.values(result);
+    console.log("CHARTS result values = " + values);
+    const keys = Object.keys(result);
+    console.log("CHARTS result keys = " + keys);
+
+    new Chart(ctxBarChart, {
+        type: 'bar',
+        data: {
+            labels: frameworks,
+            datasets: [{
+                label: 'Github Stars',
+                data: stars,
+                backgroundColor: [
+                    "rgba(255, 99, 132, 0.2)",
+                    "rgba(54, 162, 235, 0.2)",
+                    "rgba(255, 206, 86, 0.2)",
+                    "rgba(75, 192, 192, 0.2)",
+                    "rgba(153, 102, 255, 0.2)"],
+                borderColor: [
+                    "rgba(255, 99, 132, 1)",
+                    "rgba(54, 162, 235, 1)",
+                    "rgba(255, 206, 86, 1)",
+                    "rgba(75, 192, 192, 1)",
+                    "rgba(153, 102, 255, 1)",],
+                borderWidth: 1
+            }]
+        },
+    })
 
 // const lineChart = new Chart(ctxLineChart, {
 //  type: 'line',
@@ -109,18 +147,20 @@ const barChart = new Chart(ctxBarChart, {
 //  },
 // })
 
-const lineChart = new Chart(ctxLineChart, {
- type: 'line',
- data: {
-    labels: frameworks,
-    datasets: [{
-        label: 'Github Stars',
-        data: stars,
-        backgroundColor: "rgba(255, 99, 132, 0.2)",
-        borderColor: "rgba(255, 99, 132, 1)",
-        borderWidth: 1,
-        // fill: false,
-        // lineTension: 0
-    }]
- },
-})
+    new Chart(ctxLineChart, {
+        type: 'line',
+        data: {
+            labels: frameworks,
+            datasets: [{
+                label: 'Github Stars',
+                data: stars,
+                backgroundColor: "rgba(255, 99, 132, 0.2)",
+                borderColor: "rgba(255, 99, 132, 1)",
+                borderWidth: 1,
+                // fill: false,
+                // lineTension: 0
+            }]
+        },
+    })
+}
+charts()
