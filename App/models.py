d0a36f0b36514a1c177e8816f606c85cd240e751
Szymon Rogowski <szymon0r0official@gmail.com>
2023-03-03 00:21:23 +0100

    models

--- App/models.py
+++ App/models.py
@@ -1,8 +1,14 @@
-# TODO:
-# Not Finished
-# from django.db import models
-# class User(models.Model):
-#     __tablename__ = 'table'
-#     name = models.CharField(max_length=100)
-#     def __str__(self):
-#         return self.name
+# TODO: Not Finished
+# TODO: No idea what to store
+# python manage,py makemigrations App
+
+from django.db import models
+
+
+
+class User(models.Model):
+    __tablename__ = 'table'
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
