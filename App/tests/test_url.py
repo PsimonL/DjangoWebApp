6ddcc91fe123d6934f155a9e9f563979990511de
Szymon Rogowski <szymon0r0official@gmail.com>
2023-03-03 01:54:27 +0100

    Tests

--- App/tests/test_url.py
+++ App/tests/test_url.py
@@ -10,17 +10,17 @@ class TestUrls(SimpleTestCase):
     def test_setup(self):
         assert 1 == 1
 
-    def test_homepage_url_is_reversed(self):
+    def test_homepage_url_reversed(self):
         url = reverse('homepage')
         print(resolve(url))
         self.assertEquals(resolve(url).func, get_homepage)
 
-    def test_converter_url_is_reversed(self):
+    def test_converter_url_reversed(self):
         url = reverse('converter')
         print(resolve(url))
         self.assertEquals(resolve(url).func, get_converter)
 
-    def test_charts_url_is_reversed(self):
+    def test_charts_url_reversed(self):
         url = reverse('charts')
         print(resolve(url))
         self.assertEquals(resolve(url).func, get_charts)
