03d9e0967057d15458820b51c4040e00da88baf7
Szymon Rogowski <szymon0r0official@gmail.com>
2023-03-07 21:56:03 +0100

    tests

--- functional_tests/test_Converter.py
+++ functional_tests/test_Converter.py
@@ -36,7 +36,7 @@ class TestConverterHTML(StaticLiveServerTestCase):
         # Check updated image
         self.assertEqual(flag1_element.get_attribute('src'), f'https://www.countryflagicons.com/FLAT/64/{pick}.png')
 
-    # Crushes after print("2")
+
     def test_conversion_getRates(self):
         self.driver.get(self.live_server_url + '/conv')
         with open('App/static/js/converter.js', 'r') as f:
@@ -52,10 +52,52 @@ class TestConverterHTML(StaticLiveServerTestCase):
         print("apiFunc = " + str(mid))
         self.assertEqual(resultFunc, mid, "Comparison Done")
 
+    def test_insert_field(self):
+        self.driver.get(self.live_server_url + '/conv')
+        insertion = self.driver.find_element(By.ID, 'enter')
+        insertion.clear()
+        num = random.randint(20, 200)
+        insertion.send_keys(num)
+        input_value = insertion.get_attribute('value')
+        print("input_value = " + input_value)
+        self.assertEqual(int(input_value), num)
+
+    # Expected: 'Your conversion will appear here.'
+    # Actual: '100 eur => 469.00000000000006 pln'
     def test_conversion_button(self):
         self.driver.get(self.live_server_url + '/conv')
-        button = self.driver.find_element(By.ID, 'exchange-button')
+        WebDriverWait(self.driver, 10).until(
+            expected_conditions.presence_of_element_located((By.TAG_NAME, "body"))
+        )
+
+        self.driver.execute_script("document.dispatchEvent(new Event('DOMContentLoaded'));")
+
+        output = self.driver.find_element(By.ID, 'conversion')
+        print("output = " + output.text)
+
+        self.driver.refresh()
+
+        button = self.driver.find_element(By.CLASS_NAME, 'exchange-button')
         button.click()
+
+        # WebDriverWait(self.driver, 5).until(
+        #     expected_conditions.text_to_be_present_in_element_value(output, "100 eur => 469.00000000000006 pln")
+        # )
+
+        self.assertEqual(self.driver.find_element(By.ID, "conversion").text, "100 eur => 469.00000000000006 pln")
+
+
+        # self.driver.get(self.live_server_url + '/conv')
+        # insertion = self.driver.find_element(By.ID, 'enter')
+        # input_value = insertion.get_attribute('value')
+        # self.driver.refresh()
+        # button = self.driver.find_element(By.CLASS_NAME, 'exchange-button')
+        # button.click()
+        # self.driver.refresh()
+        # output = self.driver.find_element(By.ID, 'conversion')
+        # output_value = output.get_attribute('value')
+        # print("output_value = " + str(type(output_value)))
+        # self.assertEqual(f"{input_value} eur => 469.00000000000006 pln", output_value)
 
     def tearDown(self):
         self.driver.quit()
